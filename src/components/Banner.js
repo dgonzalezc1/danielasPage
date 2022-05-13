ae8d630cef30da260da76dbccd2a09df6cea6349
Daniela Gonzalez <d.gonzalezc1@uniandes.edu.co>
2022-05-13 23:53:40 +0200

    error in portfolio

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,7 +13,7 @@ function Banner() {
                         <h2>González</h2>
                     </div>
                     <div className="bannerText-sec">
-                        <p>Mechanical Engineering and Computer Science student <br/> from Colombia passionate about innovation, design, UX/UI <br/>and sustainability.</p>
+                        <p>Newly graduated Mechanical Engineering and Computer Scientist<br/> from Colombia passionate about innovation, design, UX/UI, entrepreneurship <br/>and sustainability.</p>
                     </div>
                     
                 </div>
